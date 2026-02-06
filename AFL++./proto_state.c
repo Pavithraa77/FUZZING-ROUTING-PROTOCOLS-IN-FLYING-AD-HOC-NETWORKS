994ad94a5c83b4618c71df50a3ec5bead9a19a0c
Ratakonda Bhavitha <121236276+ratakondabhavitha@users.noreply.github.com>
2026-02-06 10:35:02 +0000

    state aware

---
+++ AFL++./proto_state.c
@@ -0,0 +1,21 @@
+#include "proto_state.h"
+
+/* Current protocol state */
+static enum proto_state current_state = ST_INIT;
+
+void proto_state_reset(void)
+{
+    current_state = ST_INIT;
+}
+
+void proto_state_hit(enum proto_state s)
+{
+    if (s > current_state)
+        current_state = s;
+}
+
+/* Alias used by protocol code (batman.c) */
+void hit_state(enum proto_state s)
+{
+    proto_state_hit(s);
+}
